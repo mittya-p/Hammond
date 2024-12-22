abf08d2c4f9ca50c03f76863c087d1a62e345698
Dmitro <dm.pobezhimov@gmail.com>
2024-12-22 11:59:20 +0300

    cookie_06

--- js/cookie.js
+++ js/cookie.js
@@ -5,7 +5,7 @@ function setLanguageCookie(language) {
   }` // 1 год
 }
 
-// Перенаправление на сохранённый язык, только если текущий язык не совпадает
+// Перенаправление на сохранённый язык
 function redirectToSavedLanguage() {
   const cookies = document.cookie.split('; ')
   const languageCookie = cookies.find((row) => row.startsWith('language='))
@@ -16,6 +16,7 @@ function redirectToSavedLanguage() {
     const isEnglishPage = currentPage.includes('index_en.html')
     const isUkrainianPage = currentPage.includes('index.html') && !isEnglishPage
 
+    // Перенаправление на нужную страницу в зависимости от сохранённого языка
     if (savedLanguage === 'en' && !isEnglishPage) {
       window.location.href = './index_en.html'
     } else if (savedLanguage === 'ua' && !isUkrainianPage) {
